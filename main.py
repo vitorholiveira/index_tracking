36556968046cfa11e6e642fe662527c31f303382
Vitor Hugo Magnus Oliveira <vitorh.magnus.oliveira@gmail.com>
2024-12-06 18:28:38 -0300

    organizing directories and IndexTracker class

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 from indextracking import IndexTracking
+from dataset import build_dataset, get_tickers
 import pandas as pd
+import os
 import json
 
-
-def save_result(result, portfolio_size, max_iterations, index_ticker):
+def save_portfolio(result, filename):
     data = {
         'Error': result['error'],
-        'Portfolio Weights': json.dumps(result['portfolio']),
+        'Portfolio Weights': json.dumps(result['weights']),
         'Train Tracking Error': result['performance']['train_performance']['tracking_error'],
         'Train RMSE': result['performance']['train_performance']['root_mean_squared_error'],
         'Train Correlation': result['performance']['train_performance']['correlation'],
         'Test Tracking Error': result['performance']['test_performance']['tracking_error'],
         'Test RMSE': result['performance']['test_performance']['root_mean_squared_error'],
         'Test Correlation': result['performance']['test_performance']['correlation'],
-        'Optimization Time': result['optimization_time']
+        'Optimization Time': result['optimization_time'],
+        'Start train': result['dates']['train']['start'],
+        'End train': result['dates']['train']['end'],
+        'Start test': result['dates']['test']['start'],
+        'End test': result['dates']['test']['end'],
     }
     
     df = pd.DataFrame([data])
-    df.to_csv(f'result_{index_ticker}_{portfolio_size}stocks_{max_iterations}iterations.csv', index=False)
+    df.to_csv(filename, index=False)
 
-def treinacao(index_ticker, portfolio_size=10, max_iterations=1000000):
+def treinacao(index_ticker, portfolio_size=10, max_iterations=1000000, initial_solution=False):
     if(index_ticker=='^BVSP'):
-        data = pd.read_csv('stock_variance_^BVSP.csv', index_col=0)
+        data = pd.read_csv('./data/stock_variance_^BVSP.csv', index_col=0)
         index_data = data[index_ticker]
         stock_data = data.drop(columns=[index_ticker])
     elif(index_ticker=='^OEX'):
-        data = pd.read_csv('stock_variance_^OEX.csv', index_col=0)
+        data = pd.read_csv('./data/stock_variance_^OEX.csv', index_col=0)
         index_data = data[index_ticker]
         stock_data = data.drop(columns=[index_ticker])
     else:
@@ -41,16 +46,44 @@ def treinacao(index_ticker, portfolio_size=10, max_iterations=1000000):
 
     model = IndexTracking(stock_data=stock_data, index_data=index_data)
     model.split_data(train_start=train_start, train_end=train_end, test_start=test_start, test_end=test_end)
-    model.mult_start_optimization(num_starts=1, portfolio_size=portfolio_size, max_iterations=max_iterations)
+    portfolio = model.create_portfolio(portfolio_size=portfolio_size, max_iterations=max_iterations, initial_solution=initial_solution)
 
-    save_result(model.best_result['regular'], portfolio_size=portfolio_size, max_iterations=max_iterations, index_ticker=index_ticker)
+    if(initial_solution):
+        filename = f'./portfolios/initial_{index_ticker}_{portfolio_size}stocks.csv'
+    else:
+        filename = f'./portfolios/regular_{index_ticker}_{portfolio_size}stocks.csv'
 
-# FLORES
-bvsp_index_ticker = '^BVSP'
-sp100_index_ticker = '^OEX'
+    save_portfolio(portfolio, filename=filename)
 
-treinacao(bvsp_index_ticker, portfolio_size=10, max_iterations=10**(7))
-treinacao(sp100_index_ticker, portfolio_size=10, max_iterations=10**(7))
 
-treinacao(bvsp_index_ticker, portfolio_size=20, max_iterations=10**(7))
-treinacao(sp100_index_ticker, portfolio_size=20, max_iterations=10**(7))
+def build_dataset_from_tickers(index_ticker):
+    if os.path.exists(f'./data/stock_values_{index_ticker}.csv') and os.path.exists(f'./data/stock_variance_{index_ticker}.csv'):
+        return
+    stock_tickers = get_tickers(f'./data/tickers_{index_ticker}.csv')
+    start_date = "2023-01-01"
+    end_date = "2024-01-01"
+    build_dataset(index_ticker, stock_tickers, start_date, end_date)
+
+def main():
+    bvsp_index_ticker = '^BVSP'
+    sp100_index_ticker = '^OEX'
+
+    max_iterations = 10**(3)
+
+    build_dataset_from_tickers(bvsp_index_ticker)
+    build_dataset_from_tickers(sp100_index_ticker)
+
+    treinacao(bvsp_index_ticker, portfolio_size=10, max_iterations=max_iterations)
+    treinacao(sp100_index_ticker, portfolio_size=10, max_iterations=max_iterations)
+
+    treinacao(bvsp_index_ticker, portfolio_size=20, max_iterations=max_iterations)
+    treinacao(sp100_index_ticker, portfolio_size=20, max_iterations=max_iterations)
+
+    treinacao(bvsp_index_ticker, portfolio_size=10, max_iterations=max_iterations, initial_solution=True)
+    treinacao(sp100_index_ticker, portfolio_size=10, max_iterations=max_iterations, initial_solution=True)
+
+    treinacao(bvsp_index_ticker, portfolio_size=20, max_iterations=max_iterations, initial_solution=True)
+    treinacao(sp100_index_ticker, portfolio_size=20, max_iterations=max_iterations, initial_solution=True)
+
+if __name__ == "__main__":
+    main()
